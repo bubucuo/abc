a7e0cf84c971e6d8080cd59bc89b8ac81ea3758b
bubucuo <276185876@qq.com>
2021-11-02 17:45:34 +0800

    save

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # abc
+
 算法
+
+
+
+*Tes上怎么做链表题*：howToDoList
+
+合并两个排序的链表： mergeTwoLists 
+
+数据流中第K大元素：partialSort
+
--- howToDoList.js
+++ howToDoList.js
@@ -1,1 +1,46 @@
 // Tes上怎么做链表题
+
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+// array->list
+function generateList(array) {
+  const fakeHead = new ListNode(0);
+  let current = fakeHead;
+
+  for (let i = 0; i < array.length; i++) {
+    current.next = { val: array[i], next: null };
+    current = current.next;
+  }
+
+  return fakeHead.next;
+}
+
+// list->array
+function generateArray(list) {
+  let res = [];
+
+  while (list) {
+    res.push(list.val);
+    list = list.next;
+  }
+
+  return res;
+}
+
+let a1 = [1, 2, 3];
+let a2 = [2, 3, 4];
+
+let l1 = generateList(a1);
+let l2 = generateList(a2);
+
+console.log("l1", l1);
+console.log("l2", l1);
+
+a1 = generateArray(l1);
+a2 = generateArray(l2);
+
+console.log("a1", a1);
+console.log("a2", a2);
