f0201e00a30b8011d0cac6441b8e429b9cad609e
bubucuo <276185876@qq.com>
2021-11-02 17:21:17 +0800

    Update mergeTwoLists.js

--- mergeTwoLists.js
+++ mergeTwoLists.js
@@ -3,9 +3,11 @@ function ListNode(val) {
   this.next = null;
 }
 
-var mergeTwoLists = function (l1, l2) {
-  const listNode = new ListNode(0);
-  let current = listNode;
+function mergeTwoLists(l1, l2) {
+  let fakeHead = new ListNode(0);
+
+  let current = fakeHead;
+
   while (l1 && l2) {
     if (l1.val < l2.val) {
       current.next = l1;
@@ -16,40 +18,50 @@ var mergeTwoLists = function (l1, l2) {
     }
     current = current.next;
   }
+
   if (l1) {
     current.next = l1;
   }
+
   if (l2) {
     current.next = l2;
   }
-  return listNode.next;
-};
 
-// 输入：1->2->4, 1->3->4
-// 输出：1->1->2->3->4->4
+  return fakeHead.next;
+}
 
 function generateList(array) {
   let fakeHead = new ListNode(0);
+
   let current = fakeHead;
+
   for (let index = 0; index < array.length; index++) {
     current.next = { val: array[index], next: null };
+
     current = current.next;
   }
+
   return fakeHead.next;
 }
 
 function generateArray(list) {
   let res = [];
+
   while (list) {
     res.push(list.val);
+
     list = list.next;
   }
 
   return res;
 }
 
-const l1 = generateList([1, 2, 3]);
-const l2 = generateList([2, 3, 4]);
+let a1 = [1, 2, 3];
+let a2 = [2, 3, 4];
+
+let l1 = generateList(a1);
+let l2 = generateList(a2);
 
 const res = generateArray(mergeTwoLists(l1, l2));
-console.log("res", res); //sy-log
+
+console.log(res); //sy-log
