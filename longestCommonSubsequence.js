a7bbfc39b41e4b737b8261c3d373e1946b260fd6
bubucuo <276185876@qq.com>
2021-11-16 19:05:36 +0800

    Update longestCommonSubsequence.js

--- longestCommonSubsequence.js
+++ longestCommonSubsequence.js
@@ -5,4 +5,24 @@
  * @param {string} text2
  * @return {number}
  */
-var longestCommonSubsequence = function (text1, text2) {};
+// *时间复杂度O(mn)
+// *空间复杂度O(mn)
+
+var longestCommonSubsequence = function (text1, text2) {
+  const m = text1.length;
+  const n = text2.length;
+  let dp = [new Array(n + 1).fill(0)];
+  for (let i = 1; i <= m; i++) {
+    dp[i] = [0];
+    const c1 = text1.charAt(i - 1);
+    for (let j = 1; j <= n; j++) {
+      const c2 = text2.charAt(j - 1);
+      if (c1 === c2) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+  return dp[m][n];
+};
